52c182e08eaf8a704a016144569bc36351260bbd
Sue Richeson <spr7b@virginia.edu>
2013-12-17 13:40:07 -0500

    Added modernizr.js to config.assets.precompile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-CurateUva::Application.configure do
+Libra2::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -60,7 +60,8 @@ CurateUva::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-
+  config.assets.precompile += %w( modernizr.js )
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
