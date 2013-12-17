d0f68425a4c4b78f54845137eba0d2e62040a584
Sue Richeson <spr7b@virginia.edu>
2013-12-17 11:57:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CurateUva::Application.config.secret_key_base = 'ed0d0c8766c5d737191322ae1a4e3865c2b9111c1322f0440af5c391c5cd5b14e13659f8b6017cf3e47b15ffb872c3354c2649e746a64f0ca14bf659fb1beac0'
