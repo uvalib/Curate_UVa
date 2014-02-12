ba54b9359959dcd7f94c6cf248e87679b87841b2
Sue Richeson <spr7b@virginia.edu>
2014-02-12 09:07:49 -0500

    Update SQLite DB migrations and schema.

--- db/migrate/20131217170259_change_display_name_to_name.curate_engine.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from curate_engine (originally 20131112155553)
-class ChangeDisplayNameToName < ActiveRecord::Migration
-  def change
-    rename_column(:users, :display_name, :name)
-    add_index(:users, :name)
-  end
-end
