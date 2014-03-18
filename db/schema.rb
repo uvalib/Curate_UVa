7eb9fafd6d6c4aa6dfe7a21200754c50881935d9
Sue Richeson <spr7b@virginia.edu>
2014-03-18 15:16:51 -0400

    Updating with the latest changes from curate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211210212) do
+ActiveRecord::Schema.define(version: 20140318163505) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",     null: false
